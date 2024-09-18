20321ca66add37fa2723f157187cfa1791b63599
JonathanStoll <jonafstoll@gmail.com>
2024-09-18 12:16:14 -0300

    fix

--- util/location.js
+++ util/location.js
@@ -1,5 +1,4 @@
-const GOOGLE_API_KEY = 'AIzaSyBpxU2kM_RZ5Foo7U-1EsvfCrAvqWtX2-Q'
-
+import { GOOGLE_API_KEY } from '../constants/variabels';
 export function getMapPreview(lat,log){
 const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${log}&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${log}&key=${GOOGLE_API_KEY}`
 return imagePreviewUrl
