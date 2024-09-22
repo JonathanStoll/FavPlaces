69b0a30066eda3543f087d88d78f8d41113c033e
JonathanStoll <jonafstoll@gmail.com>
2024-09-22 14:04:43 -0300

    recibing data to list

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,6 +1,15 @@
+import { useEffect, useState } from "react"
 import PlacesList from "../components/places/PlacesList"
+import { useIsFocused } from "@react-navigation/native"
 
-function AllPlaces(){
-return <PlacesList/>
+function AllPlaces({route}){
+    const [loadedPlaces, setLoadedPlaces] = useState([]);
+  const isFocused = useIsFocused();
+    useEffect(()=>{
+        if(isFocused && route.params){
+           setLoadedPlaces(currentPlaces => [...currentPlaces, route.params.place])
+        }
+    },[isFocused,route])
+return <PlacesList places={loadedPlaces}/>
 }
 export default AllPlaces
